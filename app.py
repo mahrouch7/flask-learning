b419a4ee11fcfad736731f93ef64d11f8cd2f26f
mahrouch7 <maher.kouraichi@gmail.com>
2023-12-14 11:52:23 +0000

    tHIS IS THE FIRST COMMIT.
    IT IS A TEST

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hell_world():
+  return "Hello Maher!"
+
+
+if (__name__ == "__main__"):
+  app.run(host='0.0.0.0', debug=True)
